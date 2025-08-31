64d72b4a5a43ed394fee4259f3e945add63d8101
927623BEC152 <927623BEC152@mkce.ac.in>
2025-08-31 15:39:03 +0530

    Auto-commit: 2025-08-31 15:39:02

--- Reverseright_pattern.java
+++ Reverseright_pattern.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Reverseright_pattern {
-    public static void main(String args[]{
+    public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the row");
         int N=sc.nextInt();
@@ -13,6 +13,5 @@ public class Reverseright_pattern {
             }
             System.out.println();
         }
-    })
-    
+    }    
 }
