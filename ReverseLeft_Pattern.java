5767f417b44ccc2e68a870c510ae3170cafacd25
927623BEC152 <927623BEC152@mkce.ac.in>
2025-08-31 15:21:18 +0530

    Auto-commit: 2025-08-31 15:21:18

--- ReverseLeft_Pattern.java
+++ ReverseLeft_Pattern.java
@@ -3,6 +3,8 @@ public class ReverseLeft_Pattern {
     public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the size of row");
+    int N=sc.nextInt();
+    for(int i =N;i<)
 
     }
 }
