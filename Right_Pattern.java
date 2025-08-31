c03381b5367227cfbcbdd7cba1c584c445ff5793
927623BEC152 <927623BEC152@mkce.ac.in>
2025-08-31 10:58:58 +0530

    Auto-commit: 2025-08-31 10:58:58

--- Right_Pattern.java
+++ Right_Pattern.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class Right_Pattern{
     public static void main(String[] args){
-        Scannr
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number")
     }
 }
