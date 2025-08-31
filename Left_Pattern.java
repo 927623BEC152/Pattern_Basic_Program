99ad7fb4aa97a7ba7a37d8492ac68044f9943e55
927623BEC152 <927623BEC152@mkce.ac.in>
2025-08-31 11:09:07 +0530

    Auto-commit: 2025-08-31 11:09:07

--- Left_Pattern.java
+++ Left_Pattern.java
@@ -5,7 +5,7 @@ public class Left_Pattern{
         System.out.println("Enter the number of rows");
         int N=sc.nextInt();
         for(int i=1;i<=N;i++){
-            for(int j=1;j<=i;j++){
+            for(int j=N;j<=i;j--){
                 System.out.print("*");
             }
             System.out.println();
