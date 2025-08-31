f94a6f6fbb5164d3a3350477475ffb897edaf6bc
927623BEC152 <927623BEC152@mkce.ac.in>
2025-08-31 15:24:23 +0530

    Auto-commit: 2025-08-31 15:24:22

--- ReverseLeft_Pattern.java
+++ ReverseLeft_Pattern.java
@@ -6,9 +6,9 @@ public class ReverseLeft_Pattern {
     int N=sc.nextInt();
     for(int i=N;i>=1;i--){
         for(int j=1;j<=i;j++){
-            System.out.println("*");
+            System.out.print("*");
         }
-        System.out.print(" ");
+        System.out.println();
     }
      sc.close();
     }
