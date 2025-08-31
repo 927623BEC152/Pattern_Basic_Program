b650d23aad754432d7c717691830531fe330f28d
927623BEC152 <927623BEC152@mkce.ac.in>
2025-08-31 16:06:49 +0530

    Auto-commit: 2025-08-31 16:06:49

--- Reverseright_pattern.java
+++ Reverseright_pattern.java
@@ -8,7 +8,7 @@ public class Reverseright_pattern {
            for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
-             for(int j =1;j<i;j++){
+             for(int j =1;j<=i;j++){
                 System.out.print(" ");
             }
             
