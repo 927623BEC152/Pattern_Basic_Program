0dd9d603e14f3f35f759f37c16d001635fe7ca19
927623BEC152 <927623BEC152@mkce.ac.in>
2025-09-01 23:03:27 +0530

    completed

--- Inverted_Right_Aligned2.java
+++ Inverted_Right_Aligned2.java
@@ -1,6 +1,18 @@
 import java.util.*;
 public class Inverted_Right_Aligned2 {
     public static void main(String [] args){
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the patter : ");
+        int N=sc.nextInt();
+        for(int r=N;r>=1;r--){
+            for(int s=1;s<=N-r;s++){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=r;c++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
     }
 }
