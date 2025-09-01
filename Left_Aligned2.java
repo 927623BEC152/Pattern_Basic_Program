329f8f4f8ebd1be56f0591857c3473f2786d3d54
927623BEC152 <927623BEC152@mkce.ac.in>
2025-09-01 22:53:14 +0530

    completed

--- Left_Aligned2.java
+++ Left_Aligned2.java
@@ -4,6 +4,15 @@ public class Left_Aligned2 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the pattern:");
         int N=sc.nextInt();
-    
+        for(int r=1;r<=N;r++){
+            for(int s=1;s<=N-r;s++){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=r;c++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    sc.close();
     }
 }
