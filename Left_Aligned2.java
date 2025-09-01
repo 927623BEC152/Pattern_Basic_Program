9e0e91a11db64c642a8b339b8c5e8ec0d780ebcb
927623BEC152 <927623BEC152@mkce.ac.in>
2025-09-01 22:49:09 +0530

    somthing changed

--- Left_Aligned2.java
+++ Left_Aligned2.java
@@ -1,3 +1,9 @@
 import java.util.*;
-public class 2_Left_Aligned {
+public class Left_Aligned2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the pattern:");
+        int N=sc.nextInt();
+    
+    }
 }
