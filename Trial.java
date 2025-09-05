bb0a59dc28be1e7ce629d294028834f3b788fcb7
927623BEC152 <927623BEC152@mkce.ac.in>
2025-09-05 20:08:26 +0530

    new try

--- Trial.java
+++ Trial.java
@@ -6,21 +6,36 @@ public class Trial{
     System.out.println("Enter the size of row");
     int N=sc.nextInt();
     for(int i=1;i<=N;i++){
+            for(int j=i;j<N;j++){
+                System.out.print(" ");
+            }
+            for(int j =1;j<=i;j++){
+                System.out.print("*");
+            }
             for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();
+    }
+       /*  for(int r=N-1;r>=1;r--){
+            for(int s=1;s<=N-r;s++){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=N-(N-r);c++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
+    for(int i=1;i<=N;i++){
+            
     for(int i=N-1;i>=1;i--){
-       /*  for(int j=1;j<=N-i;j++){
-            System.out.print(" ");
-        }*/
+       
         for(int j=1;j<=i;j++){
             System.out.print("*");
         }
         System.out.println();
     }
-     sc.close();
+     sc.close();*/
     }
     
 
