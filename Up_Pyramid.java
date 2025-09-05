65a6226fb378cd5351b43d0bebf387ec160cdf64
927623BEC152 <927623BEC152@mkce.ac.in>
2025-09-05 20:12:14 +0530

    program

--- Up_Pyramid.java
+++ Up_Pyramid.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class Up_Pyramid {
     
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of row");
+    int N=sc.nextInt();
+    for(int i=1;i<=N;i++){
+            for(int j=i;j<N;j++){
+                System.out.print(" ");
+            }
+            for(int j =1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+        sc.close();
+    }
 }
