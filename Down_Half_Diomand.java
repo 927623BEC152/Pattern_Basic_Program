7e6617972b6941217eea7567e660cda0a6f742f2
927623BEC152 <927623BEC152@mkce.ac.in>
2025-09-05 20:23:21 +0530

    program for down half diomand

--- Down_Half_Diomand.java
+++ Down_Half_Diomand.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class Down_Half_Diomand {
+    
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of row");
+    int N=sc.nextInt();
+         for(int r=N;r>=1;r--){
+            for(int s=1;s<=N-r;s++){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=N-(N-r);c++){
+                System.out.print("*");
+            }
+
+        for(int j=1;j<=r-1;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    
+sc.close();
+}
     
 }
