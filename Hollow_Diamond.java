30858582e13efa45fbf7718c9c94ae6636416391
927623BEC152 <927623BEC152@mkce.ac.in>
2025-09-05 19:50:14 +0530

    Hollow diamond program

---
+++ Hollow_Diamond.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Hollow_Diamond {
+      public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the patter : ");
+        int N=sc.nextInt();
+        for(int i=1;i<=N;i++){
+            for(int j=i;j<N;j++){
+                System.out.print(" ");
+            }
+            for(int j =1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int r=N-1;r>=1;r--){
+            for(int s=1;s<=N-r;s++){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=N-(N-r);c++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+    
+}
