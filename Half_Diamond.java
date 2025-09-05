0e4080aa1eab47e8b22cb008fccd65a18b7d7992
927623BEC152 <927623BEC152@mkce.ac.in>
2025-09-05 20:03:08 +0530

    program inserted

--- Half_Diamond.java
+++ Half_Diamond.java
@@ -1,6 +1,28 @@
 import java.util.*;
 
 public class Half_Diamond {
+    
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of row");
+    int N=sc.nextInt();
+    for(int i=1;i<=N;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    for(int i=N-1;i>=1;i--){
+       /*  for(int j=1;j<=N-i;j++){
+            System.out.print(" ");
+        }*/
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+     sc.close();
+    }
 
 
 }
