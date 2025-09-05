26ffe93e8820637af1996c774756f2f68343ba31
927623BEC152 <927623BEC152@mkce.ac.in>
2025-09-05 20:26:23 +0530

    full diamond code

--- Full_Diamond.java
+++ Full_Diamond.java
@@ -1,3 +1,37 @@
+import java.util.Scanner;
+
 public class Full_Diamond {
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of row");
+    int N=sc.nextInt();
+   for(int i=1;i<=N;i++){
+            for(int j=i;j<N;j++){
+                System.out.print(" ");
+            }
+            for(int j =1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+         for(int r=N-1;r>=1;r--){
+            for(int s=1;s<=N-r;s++){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=N-(N-r);c++){
+                System.out.print("*");
+            }
+        for(int j=1;j<=r-1;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    
+sc.close();
+}
     
 }
