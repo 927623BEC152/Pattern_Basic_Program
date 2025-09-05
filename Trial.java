a5cf7d248635ccb8e0b44db6675ae2a79bf75278
927623BEC152 <927623BEC152@mkce.ac.in>
2025-09-05 20:21:31 +0530

    downfall pyramid

--- Trial.java
+++ Trial.java
@@ -5,7 +5,7 @@ public class Trial{
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the size of row");
     int N=sc.nextInt();
-    for(int i=1;i<=N;i++){
+   /*  for(int i=1;i<=N;i++){
             for(int j=i;j<N;j++){
                 System.out.print(" ");
             }
@@ -16,27 +16,22 @@ public class Trial{
                 System.out.print("*");
             }
             System.out.println();
-    }
-       /*  for(int r=N-1;r>=1;r--){
+    }*/
+         for(int r=N;r>=1;r--){
             for(int s=1;s<=N-r;s++){
                 System.out.print(" ");
             }
             for(int c=1;c<=N-(N-r);c++){
                 System.out.print("*");
             }
-            System.out.println();
-        }
-    for(int i=1;i<=N;i++){
-            
-    for(int i=N-1;i>=1;i--){
-       
-        for(int j=1;j<=i;j++){
+            // System.out.println();
+
+        for(int j=1;j<=r-1;j++){
             System.out.print("*");
         }
         System.out.println();
     }
-     sc.close();*/
-    }
     
-
+sc.close();
+}
 }
