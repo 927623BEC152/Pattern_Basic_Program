65cf2d30ceb89d7d18d4ce051ab730d640bcb0d9
927623BEC152 <927623BEC152@mkce.ac.in>
2025-09-05 19:58:52 +0530

    Hollow diamond

--- Trial.java
+++ Trial.java
@@ -5,10 +5,16 @@ public class Trial{
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the size of row");
     int N=sc.nextInt();
-    for(int i=N;i>=1;i--){
-        for(int j=1;j<=N-i;j++){
-            System.out.print(" ");
+    for(int i=1;i<=N;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
+    for(int i=N-1;i>=1;i--){
+       /*  for(int j=1;j<=N-i;j++){
+            System.out.print(" ");
+        }*/
         for(int j=1;j<=i;j++){
             System.out.print("*");
         }
