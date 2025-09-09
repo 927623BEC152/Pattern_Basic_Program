a9f7df4b9f27d283fdac8829648e8a91924a2b05
927623BEC152 <927623BEC152@mkce.ac.in>
2025-09-09 12:03:16 +0530

    hollow square pattern

--- Trial.java
+++ Trial.java
@@ -4,33 +4,17 @@ public class Trial{
     public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the size of row");
-    int N=sc.nextInt();
-   for(int i=1;i<=N;i++){
-            for(int j=i;j<N;j++){
+    int n=sc.nextInt();
+    for(int r=1;r<=n;r++){
+        for(int c=1;c<=n;c++){
+            if(r==1||c==1||r==n||c==n){
+                System.out.print("*");
+            }
+            else{
                 System.out.print(" ");
             }
-            for(int j =1;j<=i;j++){
-                System.out.print("*");
-            }
-            for(int j=1;j<=i-1;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-
-        }
-         for(int r=N-1;r>=1;r--){
-            for(int s=1;s<=N-r;s++){
-                System.out.print(" ");
-            }
-            for(int c=1;c<=N-(N-r);c++){
-                System.out.print("*");
-            }
-        for(int j=1;j<=r-1;j++){
-            System.out.print("*");
         }
         System.out.println();
     }
-    
-sc.close();
-}
+    }
 }
